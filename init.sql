d06bd6271b21d96c1696c5e7221def35be40d034
takinos73 <45160912+takinos73@users.noreply.github.com>
2025-12-06 15:08:11 +0200

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE IF NOT EXISTS tasks (
+    id SERIAL PRIMARY KEY,
+    description TEXT NOT NULL,
+    frequency VARCHAR(50),
+    last_done DATE,
+    next_due DATE,
+    status VARCHAR(20) DEFAULT 'Pending'
+);
