1bbbe97d01c651cf2fa9d8e5e92ec451aa95bc3b
takinos73 <45160912+takinos73@users.noreply.github.com>
2025-12-06 15:08:24 +0200

    Delete migrations/001_init.sql

--- migrations/001_init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS tasks (
-    id SERIAL PRIMARY KEY,
-    description TEXT NOT NULL,
-    frequency VARCHAR(50),
-    last_done DATE,
-    next_due DATE,
-    status VARCHAR(20) DEFAULT 'Pending'
-);
-
