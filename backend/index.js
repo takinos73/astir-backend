5ecf3f3c58c1e4f5512a9213f3da0d1367a340e7
takinos73 <45160912+takinos73@users.noreply.github.com>
2025-12-07 19:57:39 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,8 +21,14 @@ app.get("/", (req, res) => {
 });
 
 // Import Excel to database
-app.post("/import", async (req, res) => {  
+app.post("/import", async (req, res) => {
   try {
+
+    // Ensure machines.name is unique before inserting
+    await pool.query(`
+      ALTER TABLE machines
+      ADD CONSTRAINT IF NOT EXISTS unique_machine_name UNIQUE(name);
+    `);
 
     if (!fs.existsSync(excelFilePath)) {
       return res.status(404).json({ error: "Excel file not found!" });
