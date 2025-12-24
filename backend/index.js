e1cbf25a48ece21b104875a37325ad7da04f8897
takino73 <takinos73@gmail.com>
2025-12-25 00:34:20 +0200

    index_change_payloadFix

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,8 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // -------------------
 // Static Frontend
